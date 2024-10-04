d0b192a9e1993bf2617a48d6f029a30fa0a6cfbd
Matt Jaikaran <mattjaikaran@gmail.com>
2024-10-04 00:07:25 -0400

    added flake8, isort, updated readme with data

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -28,7 +28,6 @@ echo "migrating..."
 python3 manage.py migrate --no-input
 echo "migrate successful"
 
-# python3 manage.py loaddata projects/seed/categories.json --app projects --format=json # example seed data
 echo "creating superuser..."
 python3 manage.py create_superuser # creates superuser based on env file data
 echo "created superuser"
